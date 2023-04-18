6979f9dc9a0cfcbc97d3b6ce9ea867c071a07830
Kyle Yee <WatermaliaFTW@gmail.com>
2023-04-18 14:43:24 -0400

    google drive authentication and folder acquisition

--- src/driveupload.py
+++ src/driveupload.py
@@ -2,12 +2,28 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")
+
 drive = GoogleDrive(gauth)
 
 def uploadFile(file, dir):
-    f = drive.ListFile({"q": "mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
-    for folder in f:
-        print(folder['title'])
+    pass
 
-uploadFile("test", "test")
+def getFolders():
+    f = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    folders = dict()
+    for file in f:
+        if(file['mimeType'] == "application/vnd.google-apps.folder"):
+            folders[file['title']] = file['id']
+    return folders
