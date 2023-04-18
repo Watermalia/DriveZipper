8a196a9bb4f78b2a1bac7ab384606b1934bc3a39
Kyle Yee <WatermaliaFTW@gmail.com>
2023-04-18 17:12:37 -0400

    update drive and zip functionality

--- src/zip.py
+++ src/zip.py
@@ -6,12 +6,12 @@ def zip(dir, filename: str, format: str):
     # Normalize the path
     dir = os.path.normpath(dir)
 
-    if(format == "7z"):     # Compress using 7z format
+    if(format == ".7z"):     # Compress using 7z format
         with py7zr.SevenZipFile(f"{filename}.7z", 'w') as archive:
             archive.writeall(f'{dir}', os.path.basename(dir))
             archive.close()
-        print("Done!")
+            return os.path.join(dir, f"{filename}.7z")
 
-    elif(format == "zip"):  # Compress using zip format
+    elif(format == ".zip"):  # Compress using zip format
         shutil.make_archive(filename, 'zip', os.path.dirname(dir), os.path.basename(dir))
-        print("Done!!")
+        return os.path.join(dir, f"{filename}.zip")
