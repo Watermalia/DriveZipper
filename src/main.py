2dc8c78de7bbbbb5311288e7a731366483a8b935
Kyle Yee <WatermaliaFTW@gmail.com>
2023-05-31 10:31:37 -0400

    massive ui update

--- src/main.py
+++ src/main.py
@@ -41,56 +41,101 @@ class GUI(object):
         # Define variables here
         # -------------------------------------------------
         # bools for tickboxes
-        download_bool = tkinter.BooleanVar(value=True)
-        zip_bool = tkinter.BooleanVar(value=True)
-        upload_bool = tkinter.BooleanVar(value=True)
-        delete_bool = tkinter.BooleanVar(value=True)
+        self.download_bool = tkinter.BooleanVar(value=True)
+        self.zip_bool = tkinter.BooleanVar(value=True)
+        self.upload_bool = tkinter.BooleanVar(value=True)
+        self.delete_bool = tkinter.BooleanVar(value=True)
 
         # other variables
-        downloadInput = None
-        folders = drive.getFolders()
-        foldernames = list(folders.keys())
-        driveFolder = None
-        zipFormat = tkinter.StringVar(value=".zip")
+        self.folders = drive.getFolders()
+        self.foldernames = list(self.folders.keys())
+        self.driveFolder = None
+        self.zipFormat = tkinter.StringVar(value=".zip")
 
         # Title Label
-        title_lbl = customtkinter.CTkLabel(master=root, text = "DriveZipper", font=("Arial", 24))
-        title_lbl.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
+        self.title_label = customtkinter.CTkLabel(master=root, text = "DriveZipper", font=("Arial", 24)).place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
 
         # Download Section
-        download_checkbox = customtkinter.CTkCheckBox(master=root, text="Download", variable=download_bool)
-        download_checkbox.place(relx=0.05, rely=0.1)
-        download_entry = customtkinter.CTkEntry(master=root, width=300, placeholder_text="<Insert Magnet or Direct-Download Link>", textvariable=downloadInput)
-        download_entry.place(relx=0.25, rely=0.1)
+        self.download_checkbox = customtkinter.CTkCheckBox(master=root, text="Download", variable=self.download_bool, command=self.download_swap)
+        self.download_checkbox.place(relx=0.05, rely=0.1)
+        self.download_entry = customtkinter.CTkEntry(master=root, width=300, placeholder_text="<Insert Magnet Link>")
+        self.download_entry.place(relx=0.25, rely=0.1)
+        self.download_dir_button = customtkinter.CTkButton(master=root, text="...", width=75, command=lambda: self.ask_for_dir(self.download_entry), state="disabled")
+        self.download_dir_button.place(relx=0.8, rely=0.1)
 
         # Zipping Section
-        zip_checkbox = customtkinter.CTkCheckBox(master=root, text="Zip", variable=zip_bool)
-        zip_checkbox.place(relx=0.05, rely=0.2)
-        zip_name = customtkinter.CTkEntry(master=root, width=300, placeholder_text="<Filename for Zip>")
-        zip_name.place(relx=0.25, rely=0.2)
-        zip_format = customtkinter.CTkOptionMenu(master=root, values=[".zip", ".7z"], width=75, variable=zipFormat)
-        zip_format.place(relx=0.8, rely=0.2)
-        zip_format.set(".zip")
+        self.zip_checkbox = customtkinter.CTkCheckBox(master=root, text="Zip", variable=self.zip_bool, command=self.zip_swap)
+        self.zip_checkbox.place(relx=0.05, rely=0.2)
+        self.zip_entry = customtkinter.CTkEntry(master=root, width=300, placeholder_text="<Filename for Zip>")
+        self.zip_entry.place(relx=0.25, rely=0.2)
+        self.zip_option = customtkinter.CTkOptionMenu(master=root, values=[".zip", ".7z"], width=75, variable=self.zipFormat)
+        self.zip_option.place(relx=0.8, rely=0.2)
+        self.zip_option.set(".zip")
+        self.zip_dir_button = customtkinter.CTkButton(master=root, text="...", width=75, command=lambda: self.ask_for_dir(self.zip_entry))
 
         # Uploading Section
-        upload_checkbox = customtkinter.CTkCheckBox(master=root, text="Upload", variable=upload_bool)
-        upload_checkbox.place(relx=0.05, rely=0.3)
-        upload_dropdown = customtkinter.CTkOptionMenu(master=root, values=foldernames, width=300, variable=driveFolder)
-        upload_dropdown.place(relx=0.25, rely=0.3)
-        upload_dropdown.set("<Select Google Drive Folder>")
+        self.upload_checkbox = customtkinter.CTkCheckBox(master=root, text="Upload", variable=self.upload_bool)
+        self.upload_checkbox.place(relx=0.05, rely=0.3)
+        self.upload_dropdown = customtkinter.CTkOptionMenu(master=root, values=self.foldernames, width=300, variable=self.driveFolder)
+        self.upload_dropdown.place(relx=0.25, rely=0.3)
+        self.upload_dropdown.set("<Select Google Drive Folder>")
 
         # Delete After Section
-        delete_checkbox = customtkinter.CTkCheckBox(master=root, text="Delete After", variable=delete_bool)
-        delete_checkbox.place(relx=0.05, rely=0.4)
+        self.delete_checkbox = customtkinter.CTkCheckBox(master=root, text="Delete Zip File After", variable=self.delete_bool)
+        self.delete_checkbox.place(relx=0.05, rely=0.4)
 
-        dir_button = customtkinter.CTkButton(master=root, text="...", command=self.ask_for_dir)
-        dir_button.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+        self.ok_button = customtkinter.CTkButton(master=root, text="OK", command=self.submit)
+        self.ok_button.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
 
-        ok_button = customtkinter.CTkButton(master=root, text="OK", command=self.submit)
-        ok_button.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
+        
+    def download_swap(self):
+        self.download_entry.delete(0, END)
+        if self.download_checkbox.get():
+            self.download_entry.configure(placeholder_text = "<Insert Magnet Link>")
+            self.download_dir_button.configure(state="disabled")
+        else:
+            self.download_entry.configure(placeholder_text = "<Insert Path to Folder>")
+            self.download_dir_button.configure(state="normal")
 
-    def ask_for_dir(self):
+    def zip_swap(self):
+        if self.zip_checkbox.get():
+            self.enable_download()
+            self.zip_option.place(relx=0.8, rely=0.2)
+            self.zip_entry.configure(placeholder_text="<Filename for Zip>")
+            self.zip_dir_button.place_forget()
+        else:
+            self.disable_download()
+            self.zip_option.place_forget()
+            self.zip_entry.configure(placeholder_text="<Insert Path to File>")
+            self.zip_dir_button.place(relx=0.8, rely=0.2)
+
+    def enable_download(self):
+        self.download_checkbox.configure(state="normal")
+        self.download_entry.place(relx=0.25, rely=0.1)
+        self.download_dir_button.place(relx=0.8, rely=0.1)
+
+    def disable_download(self):
+        self.download_checkbox.deselect()
+        self.download_checkbox.configure(state="disabled")
+        self.download_entry.place_forget()
+        self.download_dir_button.place_forget()
+
+    def enable_zip(self):
+        self.zip_checkbox.configure(state="normal")
+        self.zip_entry.place(relx=0.25, rely=0.2)
+        self.zip_dir_button.place(relx=0.8, rely=0.2)
+    
+    def disable_zip(self):
+        self.zip_checkbox.deselect()
+        self.zip_checkbox.configure(state="disabled")
+        self.zip_entry.place_forget()
+        self.zip_option.place_forget()
+        self.zip_dir_button.place_forget()
+
+    def ask_for_dir(self, entry):
         dir = filedialog.askdirectory()
+        entry.delete(0, END)
+        entry.insert(0, dir)
 
     def submit(self):
         
